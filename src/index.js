bca70934b4c5a6b13784361fe1519b5436927f83
Andrej Karavida <andrejkaravida@gmail.com>
2020-04-18 02:26:04 +0200

    Async actions, root resolver

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import App from './containers/App';
+import thunkMiddleware from 'redux-thunk';
 import { Provider} from 'react-redux';
-import { createStore } from 'redux';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import { searchRobots, requestRobots } from './reducers';
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,7 +25,4 @@ ReactDOM.render(
   document.getElementById('root')
 ); 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
